dd1efd381fc554d7cf8efba475a753c533d98af7
kumar6646 <sai.penusoft@gmail.com>
2025-06-26 11:04:53 +0530

    changes made by addeding boolean type

--- ExplicitTypeCasting/Program.cs
+++ ExplicitTypeCasting/Program.cs
@@ -16,21 +16,24 @@ namespace ExplicitTypeCasting
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a character");
-            char s =Convert.ToChar(Console.ReadLine());
-            Console.WriteLine(s);
+            //Console.WriteLine("Enter a character");
+            //char s =Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(s);
 
-            Console.WriteLine("Enter a number");
-            int a=Convert.ToInt16(Console.ReadLine());
-            Console.WriteLine("value of a is" + a);
+            //Console.WriteLine("Enter a number");
+            //int a=Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("value of a is" + a);
 
-            Console.WriteLine("Enter a number");
-            decimal b=Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("value of b is"+b);
+            //Console.WriteLine("Enter a number");
+            //decimal b=Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("value of b is"+b);
 
-            Console.WriteLine("Enter a name");
-            string name=Console.ReadLine();
-            Console.WriteLine("name is"+name);
+            //Console.WriteLine("Enter a name");
+            //string name=Console.ReadLine();
+            //Console.WriteLine("name is"+name);
+
+            bool state = true;
+            Console.WriteLine(state);
         }
     }
 }
