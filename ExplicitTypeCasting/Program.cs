3c832aaf7af04bf8f4dacd6e2c9752ff498257dd
kumar6646 <sai.penusoft@gmail.com>
2025-06-26 11:00:41 +0530

    Add project files.

---
+++ ExplicitTypeCasting/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitTypeCasting
+{
+    /*
+     * Explicity type casting means converting one type to another type
+     * explicitly.
+     * Convert.ToInt
+     * Int.Parsing
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a character");
+            char s =Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(s);
+
+            Console.WriteLine("Enter a number");
+            int a=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("value of a is" + a);
+
+            Console.WriteLine("Enter a number");
+            decimal b=Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("value of b is"+b);
+
+            Console.WriteLine("Enter a name");
+            string name=Console.ReadLine();
+            Console.WriteLine("name is"+name);
+        }
+    }
+}
